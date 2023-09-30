e37a6b80eaf799c888c2b89f542687c4419f0b1c
Artem Ryazanov <artryazanov@gmail.com>
2023-09-30 17:57:40 +0300

    Simplify algorithm

--- shorts.py
+++ shorts.py
@@ -13,68 +13,21 @@ short_length = 35
 
 # Ask for video info
 video_file_path = input("\nEnter video file path >  ")
-option = input('Do you want AI to generate content? (yes/no) >  ')
-
-if option == 'yes':
-    # Generate content using OpenAI API
-    theme = input("\nEnter the theme of the video >  ")
-
-    ### MAKE .env FILE AND SAVE YOUR API KEY ###
-    openai.api_key = os.environ["OPENAI_API"]
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo-instruct",
-        prompt=f"Generate content on - \"{theme}\"",
-        temperature=0.7,
-        max_tokens=768,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    print(response.choices[0].text)
-
-    yes_no = input('\nIs this fine? (yes/no) >  ')
-    if yes_no == 'yes':
-        content = response.choices[0].text
-    else:
-        content = input('\nEnter >  ')
-else:
-    content = input('\nEnter the content of the video >  ')
 
 # Create the directory
 if not os.path.exists('generated'):
     os.mkdir('generated')
 
-# Generate speech for the video
-speech = gTTS(text=content, lang='en', tld='ca', slow=False)
-speech.save("generated/speech.mp3")
-
-audio_clip = AudioFileClip("generated/speech.mp3")
-
-if audio_clip.duration + 1.3 > short_length:
-    print('\nSpeech too long!\n' + str(audio_clip.duration) + ' seconds\n' + str(audio_clip.duration + 1.3) + ' total')
-    print('It will be cut to ' + str(short_length - 1.3) + ' seconds\n')
-    audio_clip = audio_clip.subclip(0, short_length - 1.3)
-    # exit()
-
-print('\n')
-
-### VIDEO EDITING ###
-
-gp = random.choice(["1", "2"])
 video_clip = VideoFileClip(video_file_path)
 max_start_point = int(video_clip.duration - short_length - 1.3)
 start_point = random.randint(5, max_start_point)
 
-video_clip = video_clip.subclip(start_point, start_point + audio_clip.duration + 1.3)
-video_clip = video_clip.volumex(0.25)
-final_audio = CompositeAudioClip([video_clip.audio, audio_clip])
-
-final_clip = video_clip.set_audio(final_audio)
+final_clip = video_clip.subclip(start_point, start_point + short_length + 1.3)
 
 # Resize the video to 9:16 ratio
 w, h = final_clip.size
-target_ratio = 1080 / 1920
 current_ratio = w / h
+target_ratio = 1920 / 1920
 
 if current_ratio > target_ratio:
     # The video is wider than the desired aspect ratio, crop the width
