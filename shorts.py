68d2e919fc7a2d68e8c01b414d868ff1125d8cb6
Artem Ryazanov <artryazanov@gmail.com>
2023-10-02 12:11:44 +0300

    Add background + 4:3 basic aspect ratio

--- shorts.py
+++ shorts.py
@@ -9,10 +9,13 @@ import moviepy.video.fx.crop as crop_vid
 from scenedetect import open_video, SceneManager, split_video_ffmpeg
 from scenedetect.detectors import ContentDetector, ThresholdDetector, AdaptiveDetector
 import math
+import skimage
+from scipy.ndimage import gaussian_filter
+import copy
 
 load_dotenv()
 
-short_length = 36
+SHORT_LENGTH = random.randint(25, 55)
 
 
 def detect_video_scenes(video_path, threshold=27.0):
@@ -25,28 +28,52 @@ def detect_video_scenes(video_path, threshold=27.0):
     return scene_manager.get_scene_list()
 
 
-def render_video(clip, video_file_name):
+def blur(image):
+    """ Returns a blurred (radius=8 pixels) version of the image """
+    return gaussian_filter(image.astype(float), sigma=8)
+
+
+def crop_clip(clip, target_ratio):
     # Resize the video
     w, h = clip.size
     current_ratio = w / h
-    target_ratio = 1920 / 1920
 
     if current_ratio > target_ratio:
         # The video is wider than the desired aspect ratio, crop the width
         new_width = int(h * target_ratio)
         x_center = w / 2
         y_center = h / 2
-        clip = crop_vid.crop(clip, width=new_width, height=h, x_center=x_center, y_center=y_center)
+        cropped_clip = crop_vid.crop(clip, width=new_width, height=h, x_center=x_center, y_center=y_center)
     else:
         # The video is taller than the desired aspect ratio, crop the height
         new_height = int(w / target_ratio)
         x_center = w / 2
         y_center = h / 2
-        clip = crop_vid.crop(clip, width=w, height=new_height, x_center=x_center, y_center=y_center)
+        cropped_clip = crop_vid.crop(clip, width=w, height=new_height, x_center=x_center, y_center=y_center)
 
-    # Write the final video
-    print('Render final video...')
+    return cropped_clip
+
+
+def render_video(clip, video_file_name):
     clip.write_videofile("generated/" + os.path.basename(video_file_name), codec='libx264', audio_codec='aac')
+
+
+def get_final_clip(clip, start_point, final_clip_length):
+    result_clip = clip.subclip(start_point, start_point + final_clip_length)
+
+    w, h = result_clip.size
+    if w / h > 4 / 3:
+        result_clip = crop_clip(result_clip, 4 / 3)
+
+    w, h = result_clip.size
+    if w / h > 3 / 4:
+        background_clip = crop_clip(result_clip, 9 / 16)
+        background_clip = background_clip.fl_image(blur)
+        w, h = result_clip.size
+        background_clip = background_clip.resize(width=w)
+        result_clip = CompositeVideoClip([background_clip, result_clip.set_position("center")])
+
+    return result_clip
 
 
 dir_list = os.listdir('gameplay')
@@ -65,7 +92,7 @@ for video_file in dir_list:
             i + 1,
             scene[0].get_timecode(), scene[0].get_frames(),
             scene[1].get_timecode(), scene[1].get_frames(),))
-        if (scene[1].get_seconds() - scene[0].get_seconds()) > short_length:
+        if (scene[1].get_seconds() - scene[0].get_seconds()) > SHORT_LENGTH:
             good_scene_list.append(scene)
 
     print('Good scenes list:')
@@ -80,13 +107,12 @@ for video_file in dir_list:
     if len(good_scene_list) > 0:
         for i, scene in enumerate(good_scene_list):
             min_start_point = math.floor(scene[0].get_seconds())
-            max_start_point = math.floor((scene[1].get_seconds() - short_length))
+            max_start_point = math.floor((scene[1].get_seconds() - SHORT_LENGTH))
 
-            start_point = random.randint(min_start_point, max_start_point)
-            final_clip = video_clip.subclip(start_point, start_point + short_length)
+            final_clip = get_final_clip(video_clip, random.randint(min_start_point, max_start_point), SHORT_LENGTH)
 
             split_tup = os.path.splitext(video_file)
-            render_file_name = split_tup[0] + '_scene_' + str(i) + split_tup[1]
+            render_file_name = split_tup[0] + ' scene-' + str(i) + split_tup[1]
             render_video(final_clip, render_file_name)
     else:
         if video_clip.duration < 60:
@@ -96,9 +122,6 @@ for video_file in dir_list:
             min_start_point = 12
             max_finish_point = 12
 
-        max_start_point = math.floor(video_clip.duration - short_length - max_finish_point)
-
-        start_point = random.randint(min_start_point, max_start_point)
-        final_clip = video_clip.subclip(start_point, start_point + short_length)
-
+        max_start_point = math.floor(video_clip.duration - SHORT_LENGTH - max_finish_point)
+        final_clip = get_final_clip(video_clip, random.randint(min_start_point, max_start_point), SHORT_LENGTH)
         render_video(final_clip, video_file)
